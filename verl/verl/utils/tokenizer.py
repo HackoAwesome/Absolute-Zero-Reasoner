c770c5b2923ddbe82759dcab9863f0be7649e6f0
HackoAwesome <goh8junhui8@gmail.com>
2025-05-20 14:00:09 +0800

    Update

--- verl/verl/utils/tokenizer.py
+++ verl/verl/utils/tokenizer.py
@@ -47,11 +47,13 @@ def hf_tokenizer(name_or_path, correct_pad_token=True, correct_gemma2=True, **kw
     """
     from transformers import AutoTokenizer
     if correct_gemma2 and isinstance(name_or_path, str) and 'gemma-2-2b-it' in name_or_path:
+        # Not using gemma2 for our experiment, can ignore
         # the EOS token in gemma2 is ambiguious, which may worsen RL performance.
         # https://huggingface.co/google/gemma-2-2b-it/commit/17a01657f5c87135bcdd0ec7abb4b2dece04408a
         warnings.warn('Found gemma-2-2b-it tokenizer. Set eos_token and eos_token_id to <end_of_turn> and 107.')
         kwargs['eos_token'] = '<end_of_turn>'
         kwargs['eos_token_id'] = 107
+    #Obtain tokenizer dirrectly from Hugging Face
     tokenizer = AutoTokenizer.from_pretrained(name_or_path, **kwargs)
     if correct_pad_token:
         set_pad_token_id(tokenizer)
