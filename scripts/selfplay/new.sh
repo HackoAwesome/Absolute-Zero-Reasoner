b2545840627e5bd796e0f4ed706d84a8a360437b
HackoAwesome <goh8junhui8@gmail.com>
2025-05-27 20:39:03 +0800

    Update

--- scripts/selfplay/new.sh
+++ scripts/selfplay/new.sh
@@ -22,7 +22,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     data.max_response_length=8096 \
     actor_rollout_ref.model.path=allenai/OLMo-2-0425-1B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=0 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.use_kl_loss=False \
