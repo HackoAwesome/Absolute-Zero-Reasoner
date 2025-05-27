0d8fb47628199f6415b911cb31b55dfde0bf20a7
HackoAwesome <goh8junhui8@gmail.com>
2025-05-27 20:46:13 +0800

    Update

--- scripts/selfplay/new.sh
+++ scripts/selfplay/new.sh
@@ -36,7 +36,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=64 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=64 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.max_num_batched_tokens=16384 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
