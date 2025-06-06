1a49e72413df11e387745a746b13210cb2c35f52
HackoAwesome <goh8junhui8@gmail.com>
2025-06-06 11:14:24 +0800

    Update

--- scripts/selfplay/new.sh
+++ scripts/selfplay/new.sh
@@ -39,7 +39,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.max_num_batched_tokens=256 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.1 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=False \
     actor_rollout_ref.rollout.n=1 \
